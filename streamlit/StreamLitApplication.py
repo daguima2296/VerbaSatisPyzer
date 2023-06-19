2e4f97c5c46f495f0e515207ad6bd38d0c878acf
Daniela Guisao Marin <133125427+daguima2296@users.noreply.github.com>
2023-06-19 12:07:29 +0200

    Update StreamLitApplication.py

--- streamlit/StreamLitApplication.py
+++ streamlit/StreamLitApplication.py
@@ -1230,40 +1230,65 @@ elif page == "Chatbot":
     st.markdown(
         """
         <style>
-        .button-35 {
+        .button-64 {
           align-items: center;
-          background-color: #fff;
-          border-radius: 12px;
-          box-shadow: transparent 0 0 0 3px,rgba(18, 18, 18, .1) 0 6px 20px;
-          box-sizing: border-box;
-          color: #121212;
-          cursor: pointer;
-          display: inline-flex;
-          flex: 1 1 auto;
-          font-family: Inter,sans-serif;
-          font-size: 1.2rem;
-          font-weight: 700;
-          justify-content: center;
-          line-height: 1;
-          margin: 0;
-          outline: none;
-          padding: 1rem 1.2rem;
-          text-align: center;
-          text-decoration: none;
-          transition: box-shadow .2s,-webkit-box-shadow .2s;
-          white-space: nowrap;
+          background-image: linear-gradient(144deg,#AF40FF, #5B42F3 50%,#00DDEB);
           border: 0;
+          border-radius: 8px;
+          box-shadow: rgba(151, 65, 252, 0.2) 0 15px 30px -5px;
+          box-sizing: border-box;
+          color: #FFFFFF;
+          display: flex;
+          font-family: Phantomsans, sans-serif;
+          font-size: 20px;
+          justify-content: center;
+          line-height: 1em;
+          max-width: 100%;
+          min-width: 140px;
+          padding: 3px;
+          text-decoration: none;
           user-select: none;
           -webkit-user-select: none;
           touch-action: manipulation;
+          white-space: nowrap;
+          cursor: pointer;
         }
         
-        .button-35:hover {
-          box-shadow: #121212 0 0 0 3px, transparent 0 0 0 0;
+         
+        
+        .button-64:active,
+        .button-64:hover {
+          outline: 0;
+        }
+        
+         
+        
+        .button-64 span {
+          background-color: rgb(5, 6, 45);
+          padding: 16px 24px;
+          border-radius: 6px;
+          width: 100%;
+          height: 100%;
+          transition: 300ms;
+        }
+        
+         
+        
+        .button-64:hover span {
+          background: none;
+        }
+        
+         
+        
+        @media (min-width: 768px) {
+          .button-64 {
+            font-size: 24px;
+            min-width: 196px;
+          }
         }
         </style>
     """, unsafe_allow_html=True)
-    st.markdown("        <button class='button-35' role='button' href='https://verbasatispyzer-botty.streamlit.app/'>Button 35</button>", unsafe_allow_html=True)
+    st.markdown("<button class='button-64 center' role='button'><span class='text'>Lien ChatBot</span></button>", unsafe_allow_html=True)
 elif page == "Conclusion et Perspectives":
     st.runtime.legacy_caching.clear_cache()
     st.markdown("<h4 class='center'>Conclusion</h4>",unsafe_allow_html=True)
